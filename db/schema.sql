06c33464d730bbe0ae5e5513959414fca5c6176d
smurphy7326 <smurphy7326@gmail.com>
2021-10-15 10:33:23 -0400

    seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE employee;
+CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT NOT NULL,-- Makes sure it is real
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
@@ -13,13 +13,13 @@ CREATE TABLE employee;
     PRIMARY KEY (id)
 );
 
-CREATE TABLE department;
+CREATE TABLE department (
     id INTEGER NOT NULL AUTO_INCREMENT, -- Makes sure the id is real and exists
     name VARCHAR(30) NOT NULL, -- there has to be a name and be only 30 characters
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role;
+CREATE TABLE role (
     ID INTEGER AUTO_INCREMENT,  -- makes sure it is real
     title VARCHAR(30) NOT NULL, -- 30 characters max
     salary DECIMAL (10,2) NOT NULL, -- the salary will show that there can be up to 10 numbers with 2 coming after the decimal point
--- db/seeds.sql
+++ db/seeds.sql
@@ -10,17 +10,21 @@ VALUES
     ('Legal'),
     ('Sales');
 
-INSERT INTO role
-    (title, salary departmen_id) -- these are the values that are going to be shown in the table
+INSERT INTO role 
+    (title, salary, department_id) -- these are the values that are going to be shown in the table
 VALUES
     ('Lead Engineer', 120000, 1),
     ('Engineer', 75000, 1), 
+
     ('Accountant', 84000, 2), 
     ('Financial Analyst', 67000, 2),
+
     ('HR Director', 120000, 3),
     ('HR Assistant', 40000, 3),
+
     ('Lawyer', 160000, 4),
     ('Paraleagl', 45000, 4),
+
     ('Sales Lead', 90000, 5),
     ('Salesperson', 30000, 5);
 
@@ -28,12 +32,12 @@ INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
     ('Michael', 'Brancato', 1, 1),
-    ('Richard', 'Whitehead', 2, 0),
-    ('Andi', 'Bracia', 3, 0),
-    ('Kurtus', 'Rose', 5, 0),
-    ('Brenna', 'Calderara', 6, 1),
-    ('Erin', 'Flanagan', 8, 2),
-    ('Brian', 'ODonnell', 9, 1),
-    ('Cindy', 'Mascolo', 10, 0),
-    ('Chris', 'Choinere', 11, 0),
-    ('Jen', 'Eagleson', 12, 0);
+    ('Richard', 'Whitehead', 2, 1),
+    ('Andi', 'Bracia', 3, 2),
+    ('Kurtus', 'Rose', 4, 2),
+    ('Brenna', 'Calderara', 5, 2),
+    ('Erin', 'Flanagan', 6, 2),
+    ('Brian', 'ODonnell', 7, 2),
+    ('Cindy', 'Mascolo', 8, 2),
+    ('Chris', 'Choinere', 9, 2),
+    ('Jen', 'Eagleson', 10, 2);
